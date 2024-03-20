91439269327a52df038b3f924b0bc086b5d7fdb0
dev <dev@qq.com>
2024-03-20 23:47:48 +0800

    update

--- DllLdr/Scripts/extract_clang.py
+++ DllLdr/Scripts/extract_clang.py
@@ -8,14 +8,14 @@ def main():
     parser.add_argument( '-f', required = True, help = 'Path to the source executable', type = str );
     parser.add_argument( '-o', required = True, help = 'Path to store the output raw binary', type = str );
     options = parser.parse_args();
-    print(options.f)
+    #print(options.f)
 
     with open(options.f, 'rb') as f:
         pe=pefile.PE(options.f)
         text=pe.sections[0]
-        print(text)
-        print(hex(text.PointerToRawData))
-        print(hex(text.SizeOfRawData))
+        #print(text)
+        #print(hex(text.PointerToRawData))
+        #print(hex(text.SizeOfRawData))
 
         f.seek(text.PointerToRawData)
         data = f.read(text.SizeOfRawData)
